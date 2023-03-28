bed9f7f59bc2bd7b60f768df7dc62d692fc04016
ram gawas <gawasram@gmail.com>
2023-03-28 12:36:41 +0530

    Counter

---
+++ Smart_Contracts/Counter.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: MIT
+ 
+ pragma solidity ^0.8.0;
+
+ contract Counter{
+     uint count;
+
+     function getCount() public view returns(uint) {
+         return count;
+     }
+
+     function incrementCount() public{
+         count++; 
+     }
+ }
